b9911fb614e31f8f48ea1c54dbb47f41257e3032
Muhammad Yafi <yafithekid212@gmail.com>
2016-01-27 20:06:20 +0700

    add menu button in mainmenuactivity

--- app/src/main/java/com/mandiriecash/ecashtoll/MainMenuActivity.java
+++ app/src/main/java/com/mandiriecash/ecashtoll/MainMenuActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 import com.mandiriecash.ecashtoll.dummy.ListVehicleContent;
@@ -41,10 +43,12 @@ public class MainMenuActivity extends AppCompatActivity implements VehicleFragme
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) { }
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) { }
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
         });
 
         if (savedInstanceState != null) {
@@ -90,5 +94,12 @@ public class MainMenuActivity extends AppCompatActivity implements VehicleFragme
     @Override
     public void onListFragmentInteraction(LogActivityContent.LogActivity item) {
         //TODO impl
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_menu, menu);
+        return true;
     }
 }
